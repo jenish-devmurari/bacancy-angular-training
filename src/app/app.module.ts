e771c5edf2d06563f2fa1601b24e5bc9b6906ae0
jenishdevmurari <jenish.devmurari@bacancy.com>
2024-03-27 16:46:53 +0530

    Update main

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { BlogComponent } from './blog/blog.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BlogComponent,
-    HeaderComponent,
-    FooterComponent
+   
+   
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+  
