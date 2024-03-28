f79c4b1e4f09efc44988a6bf56d71ec3fa04b9ad
jenishdevmurari <jenish.devmurari@bacancy.com>
2024-03-28 11:50:24 +0530

    Upload Task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,44 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bacancy-angular-training';
+
+  username: string = "jenish";
+
+  user = {
+    "id": 1,
+    "firstName": "Terry",
+    "lastName": "Medhurst",
+    "maidenName": "Smitham",
+    "age": 50,
+    "gender": "male",
+    "email": "atuny0@sohu.com",
+    "phone": "+63 791 675 8914",
+    "username": "atuny0",
+    "password": "9uQFF1Lh",
+    "birthDate": "2000-12-25",
+    "image": "https://robohash.org/Terry.png?set=set4",
+    "bloodGroup": "A-",
+    "height": 189,
+    "weight": 75.4,
+    "eyeColor": "Green",
+    "domain": "slashdot.org",
+    "ip": "117.29.86.254",
+    "macAddress": "13:69:BA:56:A3:74",
+    "university": "Capitol University",
+    "ein": "20-9487066",
+    "ssn": "661-64-2976",
+    "userAgent": "Mozilla/5.0 ..."
+
+  }
+
+
+
+  info: boolean = false;
+  button: string = "Hide";
+
+  toggleEffect() {
+    this.info = !this.info;
+    this.button = this.info ? "Show" : "Hide";
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -11,7 +12,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
