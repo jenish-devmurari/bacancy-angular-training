95ce45b4a0f913b6026cdb851467e92b7def4589
jenishdevmurari <jenish.devmurari@bacancy.com>
2024-04-15 17:59:33 +0530

    Routes and Activation of page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,43 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'product-list', component: ProductListComponent },
+  { path: 'product-detail/:id', component: ProductDetailsComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-   
-   
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    ProductListComponent,
+    ProductDetailsComponent,
+    NotFoundComponent,
+    NavbarComponent,
+
+
   ],
   imports: [
     BrowserModule,
-    
+    RouterModule.forRoot(appRoutes),
   ],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
-  
