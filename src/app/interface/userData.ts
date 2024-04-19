3c5506c6e588377baf6d821dd25a5324804d4d22
jenishdevmurari <jenish.devmurari@bacancy.com>
2024-04-19 19:52:54 +0530

    Completed

---
+++ src/app/interface/userData.ts
@@ -0,0 +1,10 @@
+export interface userData {
+    firstName: string;
+    lastName: string;
+    email: string;
+    phone: string;
+    password: string;
+    gender: string;
+    city: string;
+    remember: boolean;
+}
--- src/app/registration-form/registration-form.component.ts
+++ src/app/registration-form/registration-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { userData } from '../interface/userData';
 
 @Component({
   selector: 'app-registration-form',
@@ -10,8 +11,46 @@ export class RegistrationFormComponent {
 
   @ViewChild('formData') registrationForm !: NgForm;
 
-  submitForm(formValue: NgForm) {
-    console.log(this.registrationForm.form);
+  public cities = ['Ahmadabad', 'Surat', 'Rajkot', 'Vadodara'];
+  public Gender = ['Male', 'Female', 'Other'];
+
+  public userData: userData = {
+    firstName: "",
+    lastName: "",
+    email: "",
+    phone: "",
+    password: "",
+    gender: "",
+    city: "",
+    remember: false
   }
 
+  public submitForm(formValue: NgForm): void {
+    // for seeing data in console
+    console.log(this.registrationForm.form)
+  }
+
+  public resetForm(): void {
+    this.registrationForm.reset();
+  }
+
+  public setValue(): void {
+    this.registrationForm.form.setValue({
+      firstName: "Jenish",
+      lastName: "Devmurari",
+      email: "jenishdev07@gmail.com",
+      phone: "1234567890",
+      password: "Jenish@08",
+      gender: "Male",
+      city: "Surat",
+      rememberMe: true
+    })
+  }
+
+  public patchValue(): void {
+    this.registrationForm.form.patchValue({
+      gender: "Male",
+      city: "Surat",
+    });
+  }
 }
