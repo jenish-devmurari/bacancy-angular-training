70d34b7372f0f1e66bd09028b93e69a144c6a351
jenishdevmurari <jenish.devmurari@bacancy.com>
2024-05-06 17:40:28 +0530

    new branch created, install bootstrap and other dependencies that require for project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
     AppComponent,
-   
-   
+
+
   ],
   imports: [
     BrowserModule,
-    
+    BrowserAnimationsModule,
+    ToastrModule.forRoot({
+      timeOut: 2000,
+      closeButton: true,
+      progressBar: true,
+    })
+
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
-  
