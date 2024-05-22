bc2228e493ddefeb8f7f63253a5375f8ef506ece
jenishdevmurari <jenish.devmurari@bacancy.com>
2024-05-22 17:03:01 +0530

    define folder structure for task

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PostListComponent } from './post-list/post-list.component';
+import { AddPostComponent } from './add-post/add-post.component';
+import { AlertComponent } from './alert/alert.component';
+import { ConfirmComponent } from './confirm/confirm.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
+    PostListComponent,
+    AddPostComponent,
+    AlertComponent,
+    ConfirmComponent,
    
    
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule
     
   ],
   providers: [],
