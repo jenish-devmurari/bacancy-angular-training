2eb2e537356ce2e521a8376559fb6d18eb350df3
jenishdevmurari <jenish.devmurari@bacancy.com>
2024-06-03 13:35:14 +0530

    update angular sixteen to seventeen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
@@ -31,7 +31,9 @@ import { SharedModule } from './shared/shared.module';
     AppRoutingModule
 
   ],
-  providers: [],
+  providers: [
+    provideClientHydration()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
