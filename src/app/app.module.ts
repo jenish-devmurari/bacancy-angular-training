75e8412337b5fa7a7a0d15ec31ab77e67b844b13
jenishdevmurari <jenish.devmurari@bacancy.com>
2024-06-03 20:48:45 +0530

    project setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
     AppComponent,
-   
-   
+
+
   ],
   imports: [
     BrowserModule,
-    
+    BrowserAnimationsModule,
+    ToastrModule.forRoot({
+      timeOut: 2000,
+      closeButton: true,
+      progressBar: true,
+      positionClass: 'toast-top-right',
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
-  
